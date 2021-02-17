19a30742a5577ec70d7c9eb12ecf593204a41bfb
Finn Fiedler <finn.fiedler@smapiot.com>
2021-02-17 22:22:19 +0100

    nightly backup

---
+++ src/app/models/investmentCampaign.ts
@@ -0,0 +1,9 @@
+export interface InvestmentCampaign {
+  investorName: string,
+  investorLat: number;
+  investorLng: number;
+  subjectCompanyName: string;
+  subjectCompanyLat: number;
+  subjectCompanyLng: number;
+
+}
