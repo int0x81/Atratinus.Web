45dccefca073effc680fa63fc279c2124891c78b
Finn Fiedler <finn.fiedler@smapiot.com>
2021-02-19 17:46:53 +0100

    nightly backup

--- src/app/campaign-detail-modal/campaign-detail-modal.component.ts
+++ src/app/campaign-detail-modal/campaign-detail-modal.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { createChart } from 'lightweight-charts';
 
 @Component({
   selector: 'app-campaign-detail-modal',
   templateUrl: './campaign-detail-modal.component.html',
   styleUrls: ['./campaign-detail-modal.component.sass']
 })
-export class CampaignDetailModalComponent {
-
+export class CampaignDetailModalComponent implements AfterViewInit {
   constructor(public activeModal: NgbActiveModal) { }
+  ngAfterViewInit(): void {
+
+    const chartElement = document.getElementById('campaignDevelopmentChart');
+    const chart = createChart(chartElement, { width: 400, height: 300 });
+    const lineSeries = chart.addLineSeries();
+    lineSeries.setData([
+      { time: '2019-04-11', value: 80.01 },
+      { time: '2019-04-12', value: 96.63 },
+      { time: '2019-04-13', value: 76.64 },
+      { time: '2019-04-14', value: 81.89 },
+      { time: '2019-04-15', value: 74.43 },
+      { time: '2019-04-16', value: 80.01 },
+      { time: '2019-04-17', value: 96.63 },
+      { time: '2019-04-18', value: 76.64 },
+      { time: '2019-04-19', value: 81.89 },
+      { time: '2019-04-20', value: 74.43 },
+    ]);
+  }
 }
