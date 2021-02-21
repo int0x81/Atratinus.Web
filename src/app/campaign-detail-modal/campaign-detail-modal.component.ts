735909b741ef29523f52ec3fad83459ba4caec10
Finn Fiedler <finn.f@mailbox.org>
2021-02-21 09:31:42 +0100

    improved chart

--- src/app/campaign-detail-modal/campaign-detail-modal.component.ts
+++ src/app/campaign-detail-modal/campaign-detail-modal.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { AreaStyleOptions, createChart, DeepPartial, LineData, SeriesOptions, WhitespaceData } from 'lightweight-charts';
+import { AreaStyleOptions, createChart, DeepPartial, LineData, SeriesMarker, SeriesOptions, Time, WhitespaceData } from 'lightweight-charts';
 import { InvestmentCampaign } from '../models/investmentCampaign';
 import { StockDataService } from '../stock-data.service';
 
@@ -39,6 +39,10 @@ export class CampaignDetailModalComponent implements AfterViewInit {
     const chart = createChart(chartElement, { 
       width: this.currentChartWidth, 
       height: this.currentChartHeight,
+      localization: {
+        locale: 'en-US',
+        dateFormat: 'yyyy-MM-dd'
+      },
       layout: {
         fontFamily: 'Arial',
       },
@@ -47,15 +51,71 @@ export class CampaignDetailModalComponent implements AfterViewInit {
       },
       timeScale: {
         borderColor: 'rgba(197, 203, 206, 1)',
-      }
+        fixLeftEdge: true,
+      },
+      handleScroll: {
+        mouseWheel: false,
+        pressedMouseMove: false,
+      },
+      handleScale: {
+        axisPressedMouseMove: false,
+        mouseWheel: false,
+        pinch: false,
+      },
+      grid: {
+        vertLines: {
+            color: 'rgba(70, 130, 180, 0.5)',
+            style: 1,
+            visible: false,
+        },
+        horzLines: {
+            color: 'rgba(70, 130, 180, 0.5)',
+            style: 1,
+            visible: false,
+        },
+    },
     });
+
     const areaSeries = chart.addAreaSeries(areaOptions);
     areaSeries.setData(stockData);
+    const markers: SeriesMarker<Time>[] = [
+      { 
+        time: {
+          year: this.investmentCampaign.startOfCampaign.getFullYear(),
+          month: this.investmentCampaign.startOfCampaign.getMonth() + 1,
+          day: this.investmentCampaign.startOfCampaign.getDate()
+        },
+        position: 'belowBar', 
+        color: '#000000', 
+        shape: 'arrowUp', 
+        text: 'Start of Campaign' 
+      },
+      { 
+        time: {
+          year: this.investmentCampaign.endOfCampaign.getFullYear(),
+          month: this.investmentCampaign.endOfCampaign.getMonth() + 1,
+          day: this.investmentCampaign.endOfCampaign.getDate()
+        },
+        position: 'belowBar', 
+        color: '#000000', 
+        shape: 'arrowUp', 
+        text: 'End of Campaign'
+      }
+    ];
+
+    areaSeries.setMarkers(markers);
+    
+    chart.timeScale().setVisibleRange({ 
+      from: stockData[0].time,
+      to: stockData[stockData.length - 1].time
+    });
+    
 
     window.addEventListener('resize', () => {
       
       this.setChartSizes();
       chart.resize(this.currentChartWidth, this.currentChartHeight);
+      chart.timeScale().resetTimeScale();
     });
   }
 
